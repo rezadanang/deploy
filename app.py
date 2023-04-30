e3bcfc4a4243545182adf03780609f975ae7debd
rezadanang <rezadanang23@gmail.com>
2023-04-30 14:54:58 +0700

    add smote

--- app.py
+++ app.py
@@ -22,7 +22,13 @@ nltk.download('wordnet')
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix# Performance Metrics  
+from wordcloud import WordCloud,STOPWORDS
+from nltk import SnowballStemmer
+
+from sklearn.model_selection import train_test_split # Split Data 
+from imblearn.over_sampling import SMOTE # Handling Imbalanced
+
 from google_play_scraper import Sort, reviews_all
 
 
@@ -94,9 +100,48 @@ def scrape_google_play(country, start_date, end_date):
     plt.show()
     st.set_option('deprecation.showPyplotGlobalUse', False)
     st.pyplot()
+
+    #wordcloud negatif
+    df_p=df[df['sentiment']==1]
+    all_words_lem = ' '.join([word for word in df_p['content']])
+    wordcloud = WordCloud(background_color='white', width=800, height=500, random_state=21, max_font_size=130).generate(all_words_lem)
+
+    plt.figure(figsize=(20, 10))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot()
+
+    #wordcloud netral
+    df_p=df[df['sentiment']==0]
+    all_words_lem = ' '.join([word for word in df_p['content']])
+    wordcloud = WordCloud(background_color='white', width=800, height=500, random_state=21, max_font_size=130).generate(all_words_lem)
+
+    plt.figure(figsize=(20, 10))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot()
     # return df
 
+    X = df['content']
+    y = df['sentiment']
+    tfid = TfidfVectorizer()
+    X_final = tfid.fit_transform(X)
 
+    smote = SMOTE()
+    x_sm,y_sm = smote.fit_resample(X_final,y)
+
+    X_train , X_test , y_train , y_test = train_test_split(x_sm , y_sm , test_size=0.1,random_state=3)
+
+    random_forest_classifier = RandomForestClassifier()
+    random_forest_classifier.fit(X_train,y_train)
+    RandomForestClassifier()
+
+    random_forest_classifier_prediction =  random_forest_classifier.predict(X_test)
+    accuracy_score(random_forest_classifier_prediction,y_test)
 def predict_sentiment(text, model):
     # Melakukan preprocessing teks
     text = text.lower()
